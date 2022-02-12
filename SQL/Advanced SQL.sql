e5dc697ed05717687ac04acc6378af43bce688da
Leandro Gois <leandro.deoxys@gmail.com>
2022-02-11 22:37:01 -0300

    Advanced SQL Solves

---
+++ SQL/Advanced SQL.sql
@@ -0,0 +1,34 @@
+-- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+-- Equilateral: It's a triangle with  sides of equal length.
+-- Isosceles: It's a triangle with  sides of equal length.
+-- Scalene: It's a triangle with  sides of differing lengths.
+-- Not A Triangle: The given values of A, B, and C don't form a triangle.
+SELECT CASE
+          WHEN (((A + B) <= C) OR ((A + C) <= B) OR ((B + C) <= A)) THEN
+           'Not A Triangle'
+          WHEN (A = B) AND (B = C) THEN
+           'Equilateral'
+          WHEN ((A = B) AND (B <> C)) OR ((A <> B) AND (B = C)) OR ((A <> B) AND (A = C)) THEN
+           'Isosceles'
+          WHEN (A <> B) AND (B <> C) AND (A <> C) THEN
+           'Scalene'
+       END
+FROM TRIANGLES;
+
+
+/*Generate the following two result sets:
+
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+*/
+Note: There will be at least two entries in the table for each type of occupation.
+SELECT NAME || '(' || SUBSTR(OCCUPATION, 0, 1) || ')'
+FROM OCCUPATIONS
+ORDER BY NAME;
+SELECT 'There are a total of ' || COUNT(1) || ' ' || LOWER(A.OCCUPATION) || 's.'
+FROM OCCUPATIONS A
+GROUP BY A.OCCUPATION
+ORDER BY COUNT(1), A.OCCUPATION;
